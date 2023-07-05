8f7edd82b3b2c0b12c9141c5683e4d29c6742bb4
MAKHUVELE <bonganeduma@icloud.com>
2023-07-05 16:13:29 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,6 +5,18 @@
  * @s: input
  * Return: Always 0 (Success)
  */
+void _puts_recursion(char *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("First, solve the problem. Then, write the code");
+	return (0);
+}
 void _puts_recursion(char *s)
 {
 	if (*s)
